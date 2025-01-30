cbcf719111f9983a121c4b5e91f7e4b41a44a584
arba1122 <arbanadauti@hotmail.com>
2025-01-30 11:11:43 +0100

    Added Rental Class

---
+++ Rental.cs
@@ -0,0 +1,8 @@
+public class Rental
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int MovieId { get; set; }
+    public DateTime RentalDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+}
