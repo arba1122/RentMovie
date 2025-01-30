0b7c8e695672a696230dc140f4b581e1a95b5559
arba1122 <arbanadauti@hotmail.com>
2025-01-30 10:06:15 +0100

    Added Movie class

---
+++ Movie.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Movie
+{
+   
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Genre { get; set; }
+
+}
+  
