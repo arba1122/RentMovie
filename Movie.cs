b17dadcb50d0f5a4fe130320a2e17ca816dbd2ee
arba1122 <arbanadauti@hotmail.com>
2025-01-30 11:01:33 +0100

    Update Movie class with rentalprice

--- Movie.cs
+++ Movie.cs
@@ -1,12 +1,7 @@
-using System;
-
 public class Movie
 {
-   
+    public int Id { get; set; }
     public string Title { get; set; }
-    public string Director { get; set; }
-    public int ReleaseYear { get; set; }
     public string Genre { get; set; }
-
-}
-  
+    public DateTime ReleaseDate { get; set; }
+    public decimal RentalPrice { get; set; }
